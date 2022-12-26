b85f0b73c2fcb200fd0d45b488807273bae03caa
Blair Fernandes <31095601+blair49@users.noreply.github.com>
2022-12-26 18:36:28 +0530

    Setup for deployment

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@ function getWeather() {
   const address = document.getElementById("addressInput").value;
   const responseElement = document.getElementById("response");
   responseElement.innerText = "Fetching weather info...";
-  fetch(`http://localhost:3000/weather?address=${address}`).then((response) => {
+  fetch(`/weather?address=${address}`).then((response) => {
     response.json().then((data) => {
       let output;
       if (data.error) output = "Error: " + data.error;
